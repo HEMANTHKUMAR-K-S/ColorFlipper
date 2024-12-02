796ba7ccd5714768e33a6de71c072bac01f98f70
HEMANTHKUMAR K S <160527273+HEMANTHKUMAR-K-S@users.noreply.github.com>
2024-12-02 22:35:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const typecolor1 = document.getElementsByClassName('button-type')[0];
+ function changeColor(name) {
+    typecolor1.style.backgroundColor = name;
+ }
+
+ function randomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    typecolor1.style.backgroundColor = "#" + randomColor;
+ }
